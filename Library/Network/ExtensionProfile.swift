SagerNet/sing-box-for-apple

Add iOS 18 control widget

Library/Network/ExtensionProfile.swift
@@ -3,6 +3,8 @@ import Libbox
 import NetworkExtension
 
 public class ExtensionProfile: ObservableObject {
+    public static let controlKind = "io.nekohasekai.sfa.widget.ServiceToggle"
+
     private let manager: NEVPNManager
     private var connection: NEVPNConnection
     private var observer: Any?

The-boond/ClashNL

Library/Network/ExtensionProfile.swift
@@ -3,6 +3,8 @@ import Libbox
 import NetworkExtension
 
 public class ExtensionProfile: ObservableObject {
+    public static let controlKind = "io.nekohasekai.sfavt.widget.ServiceToggle"
+
     private let manager: NEVPNManager
     private var connection: NEVPNConnection
     private var observer: Any?